0318e23b61bfcd488ef3ddd95aaeeb56e92d2276
Mark Baker <user.email>
2021-07-06 12:05:54 -0500

    context

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,3 +1,33 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const FavoriteContext = createContext();
+const FavoriteContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler() {}
+
+  function itemIsFavoriteHandler() {}
+
+  function itemIsFavoriteHandler() {}
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
